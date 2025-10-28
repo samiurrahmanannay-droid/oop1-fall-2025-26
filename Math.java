738f36a1ec8453479de9fc3cc2fe1b5bf616dab2
samiurrahman <samiurrahmanannay@gmail.com>
2025-10-28 17:38:49 +0600

    25-61879-2

---
+++ Math.java
@@ -0,0 +1,9 @@
+public class Math{
+    Run|Debug
+    public static void main(string[]args){
+        int num1;
+        int num2;
+        int sum=(num1+num2)/2;
+        System.out.println(avg);
+    }
+}
